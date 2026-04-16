6f6e3b0864d933dff696288bfffeb92af16ce855
meerccie <merzkey1@gmail.com>
2026-04-16 16:06:22 +0800

    feat(dashboard) - top selling product endpoint

---
+++ Core/Application/Features/Dashboard/Queries/GetTopSellingProductsHandler.cs
@@ -0,0 +1,14 @@
+﻿using Application.Dto;
+using Application.Interfaces.Repositories;
+using MediatR;
+
+namespace Application.Features.Dashboard.Queries;
+
+public class GetTopSellingProductsHandler(IDashboardRepository dashboardRepo)
+    : IRequestHandler<GetTopSellingProductsQuery, IEnumerable<TopProductResponse>>
+{
+    public async Task<IEnumerable<TopProductResponse>> Handle(GetTopSellingProductsQuery request, CancellationToken ct)
+    {
+        return await dashboardRepo.GetTopSellingProductsAsync(request.Count, ct);
+    }
+}
---
+++ Core/Application/Features/Dashboard/Queries/GetTopSellingProductsQuery.cs
@@ -0,0 +1,6 @@
+﻿using Application.Dto;
+using MediatR;
+
+namespace Application.Features.Dashboard.Queries;
+
+public record GetTopSellingProductsQuery(int Count = 5) : IRequest<IEnumerable<TopProductResponse>>;
