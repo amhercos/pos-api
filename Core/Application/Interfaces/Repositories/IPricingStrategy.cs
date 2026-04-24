ac58a57dcf058e2e589e358835348a9613a17db2
Amher Cosaine <amhercosainetimpahan@gmail.com>
2026-04-24 23:26:13 +0800

    Refactor pricing logic to use strategy pattern
    Introduce IPricingStrategy and implementations for Bulk, Discount, and Bundle promotions. Update

---
+++ Core/Application/Interfaces/Repositories/IPricingStrategy.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using Domain.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Interfaces.Repositories
+{
+    public interface IPricingStrategy
+    {
+        PromotionType Type { get; }
+        decimal Calculate(Product product, Promotion promo, int quantity);
+    }
+}
